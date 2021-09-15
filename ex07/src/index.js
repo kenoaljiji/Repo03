fae8a0a4b8c7c6030f8cbcec652c4a7d172b59f1
kenoaljiji <studioaljiji@gmail.com>
2021-09-15 16:49:25 +0200

    writing variable name in cameCase

---
+++ ex07/src/index.js
@@ -0,0 +1,25 @@
+function main() {
+  //Only change code above this line
+  //variable declarations
+
+  var studlyCapVar;
+  var properCamelCase;
+  var titleCaseOver;
+
+  //variable assignments
+
+  studlyCapVar = 10;
+  properCamelCase = "A string";
+  titleCaseOver = 9000;
+
+  //Only change code above this line
+
+  return {
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver,
+  };
+}
+console.log(main());
+
+module.exports = main;
