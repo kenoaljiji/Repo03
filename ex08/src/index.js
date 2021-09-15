e9cda1c979f505648094eab5f0de3bb30c0e4677
kenoaljiji <studioaljiji@gmail.com>
2021-09-15 17:05:21 +0200

    sum two numbers

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+  //Only change code above this line
+  //Add two numbers
+  var sum = numOne + numTwo;
+
+  //Only change code above this line
+
+  return sum;
+}
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+module.exports = main;
