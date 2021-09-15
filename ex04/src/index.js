cb04b8c56f66b5331ab29b3c4cb4f7054a31a530
kenoaljiji <studioaljiji@gmail.com>
2021-09-15 14:39:39 +0200

    Assinging a value x to y variable

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+  //Only change code below this line
+  //Create variable with var keywoard
+
+  var x = 10;
+  var y = x;
+
+  console.log(x, y);
+
+  return [x, y];
+}
+main();
+
+module.exports = main;
