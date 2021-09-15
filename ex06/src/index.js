f93bf8442f84d7a91ef11a4f2c92b76d44c56342
kenoaljiji <studioaljiji@gmail.com>
2021-09-15 16:08:28 +0200

    Define variable and add value to it

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+  //intitialize 3 variable and add a value
+  var x = 7;
+  var y = 12;
+  var z;
+
+  z = "My name is";
+
+  //Only change code above this line
+  console.log(x);
+  console.log(y);
+  console.log(z);
+  return [x, y, z];
+}
+main();
+
+module.exports = main;
