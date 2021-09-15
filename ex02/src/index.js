0528ba81c9764780b744702bcf3c7f53f9911827
kenoaljiji <studioaljiji@gmail.com>
2021-09-15 14:10:01 +0200

    Practicing to write comment in index js file

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+  //This is comment for single line of tekst
+  /*This is comment for multi line comment  
+    console.log('First task)
+    console.log('Second task)
+    console.log('Third task)
+    console.log('4th task)
+    console.log('5th task)
+  */
+}
+main();
+
+module.exports = main;
