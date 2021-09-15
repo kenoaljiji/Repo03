5e7d0a5afa4d76b3b9fff4f021ea16d3827a3049
kenoaljiji <studioaljiji@gmail.com>
2021-09-15 15:47:04 +0200

    define variable x and add value to it

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  //intitialize variable and add a value 8
+
+  var x = 8;
+  console.log(x);
+  //Only change code below this line
+  return x;
+}
+main();
+
+module.exports = main;
